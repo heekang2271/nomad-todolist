38e418824bb4d5f98d9c9aed8252b58b9d7cf162
heekang2271 <gmlrkd3922@gmail.com>
2021-09-06 00:57:17 +0900

    시간, 스타일 수정

--- src/js/clock.js
+++ src/js/clock.js
@@ -16,8 +16,9 @@ function setDate(now) {
 function setClock(now) {
     const hours = String(now.getHours()).padStart(2, "0");
     const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
 
-    time.innerText = `${hours}:${minutes}`;
+    time.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 function setNow() {
